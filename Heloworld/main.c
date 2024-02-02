7320f3c40af8f32f88fb21166a593885f3891000
DOMINIC MATIVO <158152569+DOMINICMATIVO@users.noreply.github.com>
2024-02-02 10:31:41 -0800

    Add files via upload

---
+++ Heloworld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("MY NAME IS MATIVO!\n");
+    printf("MY COURSE IS CIVIL ENGINEERING!\n");
+    printf("MY TEAM IS MAN UNITED!\n");
+    printf("MY COUNTRY IS KENYA!\n");
+    printf("MY SCHOOL IS JKUAT!\n");
+    return 0;
+}
